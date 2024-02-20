ed8decfad12bbaea6e9160a832fbe223a1433ada
Sebastian Komorowski <sebastian.komorowski1@outlook.com>
2024-02-20 20:09:28 +0100

    added start workspace

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,21 @@
-export default function App() {
-  return <></>;
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { toMain } from "./routes";
+import LayoutPage from "./pages/LayoutPage";
+import MainPage from "./pages/MainPage";
+
+const router = createBrowserRouter([
+  {
+    path: toMain(),
+    element: <LayoutPage />,
+    children: [
+      {
+        path: toMain(),
+        element: <MainPage />,
+      },
+    ],
+  },
+]);
+
+export function App() {
+  return <RouterProvider router={router} />;
 }
---
+++ src/pages/LayoutPage.tsx
@@ -0,0 +1,5 @@
+import MainPage from "./MainPage";
+
+export default function LayoutPage() {
+  return <MainPage />;
+}
