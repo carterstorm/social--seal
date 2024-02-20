adea20bb0e030b996e1b8794798cf3e038eb2bbd
Sebastian Komorowski <sebastian.komorowski1@outlook.com>
2024-02-20 21:34:22 +0100

    created data for SocialButtons in constants file

---
+++ src/constants.ts
@@ -0,0 +1,39 @@
+import FacebookSvg from "./assets/facebook_svg.svg";
+import YoutubeSvg from "./assets/youtube_svg.svg";
+import InstagramSvg from "./assets/instagram_svg.svg";
+import LinkedInSvg from "./assets/linkedin_svg.svg";
+import GoogleSvg from "./assets/google_svg.svg";
+import TwitterSvg from "./assets/twitter_svg.svg";
+
+export const socials = [
+  {
+    id: 1,
+    socialImage: LinkedInSvg,
+    path: "https://www.linkedin.com/",
+  },
+  {
+    id: 2,
+    socialImage: FacebookSvg,
+    path: "https://www.facebook.com/",
+  },
+  {
+    id: 3,
+    socialImage: InstagramSvg,
+    path: "https://www.instagram.com/",
+  },
+  {
+    id: 4,
+    socialImage: TwitterSvg,
+    path: "https://twitter.com/",
+  },
+  {
+    id: 5,
+    socialImage: GoogleSvg,
+    path: "https://www.google.com/",
+  },
+  {
+    id: 6,
+    socialImage: YoutubeSvg,
+    path: "https://www.youtube.com/",
+  },
+];
